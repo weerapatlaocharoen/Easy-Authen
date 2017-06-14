5b9c6a5fd0b410d76ad6afb221f63397d370f91d
Student01 <weerapatonline@gmail.com>
2017-06-14 10:30:58 +0700

    Check User OK

--- Easy Authen/ViewController.swift
+++ Easy Authen/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //Implicit ประกาศตัวแปร
+    var strUser: String?
+    var strpass: String?
+    
+    
+    
+    
+    
     @IBOutlet weak var userTextField: UITextField!
     
     
@@ -20,17 +28,55 @@ class ViewController: UIViewController {
     
     
     @IBAction func loginButton(_ sender: Any) {
-    }
+        
+        //Get Value From TextField
+        strUser = userTextField.text
+    strpass = passwordTextField.text
+        
+        print("User ==> \(strUser!)")
+        print("Password ==> \(strpass!)")
+        
+        //Find Count String
+        let intUser = strUser?.characters.count
+        let intPass = strpass?.characters.count
+        
+        print("intUser ==> \(intUser!)")
+        print("intpass ==> \(intPass!)")
+        
+        //Call CheckSpace
+        if CheckSpace(myString: strUser!) {
+            print("User OK")
+        }else
+        {
+            print("User No OK Blank User")
+        }
+        
+        
+    }//ปุ่ม Login
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    } //Mian Method
+    
+    
+    func CheckSpace(myString: String) -> Bool {
+        let intString = myString.characters.count
+        var result: Bool = true
+        
+        if intString == 0 {
+            // No Space
+            result = false
+        }
+        
+        return result
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    } //didReceive
 
 
 }
