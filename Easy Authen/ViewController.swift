d901864acdffecbad21fe2882d11be14f9c38a79
Student01 <weerapatonline@gmail.com>
2017-06-14 09:30:47 +0700

    initial view

--- Easy Authen/ViewController.swift
+++ Easy Authen/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+    @IBAction func loginButton(_ sender: Any) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
